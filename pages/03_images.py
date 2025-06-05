19edefecdbcb752dcd88ff97dbb9d6e863c0c3ca
MixVanstrappon <deltairis0805@gmail.com>
2025-06-05 15:52:07 +0900

    Update 03_images.py

--- pages/03_images.py
+++ pages/03_images.py
@@ -1,2 +1,3 @@
 import streamlit as st
-st.image(https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTNqOXg3ZXJneGRla2wzczBnd3RzcDVrc3VhYm41NTNyMnNwcjFqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aFTt8wvDtqKCQ/giphy.gif)
+gif_url = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTNqOXg3ZXJneGRla2wzczBnd3RzcDVrc3VhYm41NTNyMnNwcjFqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aFTt8wvDtqKCQ/giphy.gif"
+st.image(gif_url, use_column_width=True)
