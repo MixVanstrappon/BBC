006f9e198653af4e831a8475bd4c90233f36086e
MixVanstrappon <deltairis0805@gmail.com>
2025-06-05 15:54:31 +0900

    Delete pages/03_images.py

--- pages/03_images.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-gif_url = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTNqOXg3ZXJneGRla2wzczBnd3RzcDVrc3VhYm41NTNyMnNwcjFqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aFTt8wvDtqKCQ/giphy.gif"
-st.image(gif_url, use_column_width=True)
