ecf563b88f0bfc8081a25fc590168b7051c8c9fc
MixVanstrappon <deltairis0805@gmail.com>
2025-06-05 15:48:38 +0900

    Create 03_images.py

---
+++ pages/03_images.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.image(import streamlit as st
+st.image(https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTNqOXg3ZXJneGRla2wzczBnd3RzcDVrc3VhYm41NTNyMnNwcjFqMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/aFTt8wvDtqKCQ/giphy.gif)
